cc71a382c6357c95bb71c279b60864fa60a1ba26
Florian Moser <git@famoser.ch>
2023-04-07 15:42:50 +0200

    Fix phpstan errors

--- src/Utils/SlimExtensions.php
+++ src/Utils/SlimExtensions.php
@@ -12,14 +12,21 @@
 namespace Famoser\PolyasVerification\Utils;
 
 use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Exception\HttpInternalServerErrorException;
 
 class SlimExtensions
 {
     public const STATUS_OK = 200;
+    public const STATUS_INTERNAL_SERVER_ERROR = 500;
 
-    public static function createJsonResponse(Response $response, array $body, int $statusCode = self::STATUS_OK): Response
+    public static function createJsonResponse(Request $request, Response $response, mixed $body, int $statusCode = self::STATUS_OK): Response
     {
         $jsonContent = json_encode($body);
+        if (!$jsonContent) {
+            throw new HttpInternalServerErrorException($request, 'cannot serialize to json');
+        }
+
         $response->getBody()->write($jsonContent);
 
         return $response
