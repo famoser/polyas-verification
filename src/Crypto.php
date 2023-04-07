8ff028bba00e7ef20c34858cdf2b71c263ced0ee
Florian Moser <git@famoser.ch>
2023-04-07 20:45:00 +0200

    Add test to verify RSA signature

--- src/Crypto.php
+++ src/Crypto.php
@@ -13,8 +13,54 @@ namespace Famoser\PolyasVerification;
 
 class Crypto
 {
-    public static function verifyFingerprintSignature(string $fingerprint, string $signature, string $publicKey): bool
+    public static function verifyRSASHA256Signature(string $data, string $signature, string $publicKey): bool
     {
+        if (openssl_verify($data, $signature, $publicKey, 'sha256WithRSAEncryption')) {
+            return true;
+        }
+
         return false;
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public static function generateRSAKey(int $keyBits): \OpenSSLAsymmetricKey
+    {
+        $options = [
+            'private_key_bits' => $keyBits,
+            'private_key_type' => OPENSSL_KEYTYPE_RSA,
+        ];
+        $pkey = openssl_pkey_new($options);
+        if (!$pkey) {
+            throw new \Exception('key generation failed');
+        }
+
+        return $pkey;
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public static function getPublicKeyPem(\OpenSSLAsymmetricKey $key): string
+    {
+        $keyDetails = openssl_pkey_get_details($key);
+        if (!$keyDetails) {
+            throw new \Exception('failed to get key details');
+        }
+
+        return $keyDetails['key'];
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public static function signSha256(string $data, \OpenSSLAsymmetricKey $key): string
+    {
+        if (!openssl_sign($data, $signature, $key, OPENSSL_ALGO_SHA256)) {
+            throw new \Exception('signature failed');
+        }
+
+        return $signature;
     }
 }
--- tests/Utils/CryptoTest.php
+++ tests/Utils/CryptoTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the polyas-verifier project.
+ *
+ * (c) Florian Moser <git@famoser.ch>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Famoser\PolyasVerification\Test\Utils;
 
 use Famoser\PolyasVerification\Crypto;
@@ -7,8 +16,21 @@ use PHPUnit\Framework\TestCase;
 
 class CryptoTest extends TestCase
 {
+    public const TEST_RSA_KEY_BITS = 1024; // DANGER: Too small size for production use.
+    /**
+     * @throws \Exception
+     */
     public function testFingerprintVerifies(): void
     {
-        $this->assertFalse(Crypto::verifyFingerprintSignature('', '', ''));
+        $data = 'some data, still needs to be hashed';
+
+        $key = Crypto::generateRSAKey(self::TEST_RSA_KEY_BITS);
+        $signature = Crypto::signSha256($data, $key);
+        $publicKey = Crypto::getPublicKeyPem($key);
+
+        $this->assertTrue(Crypto::verifyRSASHA256Signature($data, $signature, $publicKey));
+        $this->assertFalse(Crypto::verifyRSASHA256Signature(hash('sha256', $data), $signature, $publicKey));
+        $this->assertFalse(Crypto::verifyRSASHA256Signature($data, hash('sha256', $signature), $publicKey));
+        $this->assertFalse(Crypto::verifyRSASHA256Signature($data, $signature, hash('sha256', $publicKey)));
     }
 }
