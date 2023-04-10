a28f16437ab695548b3cb301258f87badf7aee69
Florian Moser <git@famoser.ch>
2023-04-10 17:05:08 +0200

    Improve openSSL bug documentation

--- tests/Utils/OpenSSLBugs.php
+++ tests/Utils/OpenSSLBugs.php
@@ -15,15 +15,32 @@ use PHPUnit\Framework\TestCase;
 
 class OpenSSLBugs extends TestCase
 {
-    public function testPEMError(): void
+    public function testPEMErrorUsingGetPublickey(): void
     {
         $data = 'some string';
 
         $key = openssl_pkey_new();
-        $publicKey = openssl_pkey_get_details($key)['key']; // @phpstan-ignore-line
+        $publicKeyPem = openssl_pkey_get_details($key)['key']; // @phpstan-ignore-line
         openssl_sign($data, $signature, $key, OPENSSL_ALGO_SHA256); // @phpstan-ignore-line
-        openssl_verify($data, $signature, $publicKey, 'sha256WithRSAEncryption');
 
+        // BUG: parsing the public key is successful, but an error is logged nonetheless
+        $publicKey = openssl_get_publickey($publicKeyPem);
+        $this->assertNotEquals(false, $publicKey); // publicKey is extracted successfully (you may also check by passing this to openssl_verify
+        $this->assertEquals('error:0480006C:PEM routines::no start line', openssl_error_string());
+        $this->assertFalse(openssl_error_string());
+    }
+
+    public function testPEMErrorUsingSign(): void
+    {
+        $data = 'some string';
+
+        $key = openssl_pkey_new();
+        $publicKeyPem = openssl_pkey_get_details($key)['key']; // @phpstan-ignore-line
+        openssl_sign($data, $signature, $key, OPENSSL_ALGO_SHA256); // @phpstan-ignore-line
+
+        // BUG: Verifying the signature is successful, but an error is logged nonetheless
+        $result = openssl_verify($data, $signature, $publicKeyPem, 'sha256WithRSAEncryption');
+        $this->assertEquals(1, $result); // signature OK
         $this->assertEquals('error:0480006C:PEM routines::no start line', openssl_error_string());
         $this->assertFalse(openssl_error_string());
     }
