75f1e923c20cf534e500298441ab8481b3860754
Florian Moser <git@famoser.ch>
2023-07-28 16:29:59 +0200

    feat: Add algorithm 2

---
+++ src/Crypto/POLYAS/NumbersFromSeed.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * This file is part of the polyas-verifier project.
+ *
+ * (c) Florian Moser <git@famoser.ch>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Famoser\PolyasVerification\Crypto\POLYAS;
+
+readonly class NumbersFromSeed
+{
+    public function __construct(private string $seed, private int $bitLength, private int $iteration = 1)
+    {
+    }
+
+    public function number(): \GMP
+    {
+        if (0 === $this->bitLength) {
+            return gmp_init(0);
+        }
+
+        $keyDerivationKey = $this->seed.pack('N', $this->iteration);
+        $byteLength = (int) ceil($this->bitLength / 8.0);
+        $keyDerivation = new KeyDerivation($keyDerivationKey, $byteLength, 'generator', 'Polyas');
+
+        $key = $keyDerivation->derive();
+        $positiveKey = "\0".$key;
+
+        // cut off top bits if necessary
+        if ($this->bitLength % 8 > 0) {
+            $firstByte = substr($positiveKey, 0, 2);
+            $firstNumber = unpack('n', $firstByte)[1]; // @phpstan-ignore-line
+
+            $keepBits = $this->bitLength % 8;
+            $bitsToClear = $firstNumber >> $keepBits;
+            $firstNumberWithClearedBits = $firstNumber ^ ($bitsToClear << $keepBits);
+
+            $clearedFirstByte = pack('n', $firstNumberWithClearedBits);
+            $positiveKey = $clearedFirstByte.substr($key, 2);
+        }
+
+        return gmp_import($positiveKey);
+    }
+
+    public function iterate(): NumbersFromSeed
+    {
+        return new NumbersFromSeed($this->seed, $this->bitLength, $this->iteration + 1);
+    }
+}
---
+++ tests/Crypto/POLYAS/NumberFromSeedTest.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * This file is part of the polyas-verifier project.
+ *
+ * (c) Florian Moser <git@famoser.ch>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Famoser\PolyasVerification\Test\Crypto\POLYAS;
+
+use Famoser\PolyasVerification\Crypto\POLYAS\NumbersFromSeed;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
+
+class NumberFromSeedTest extends TestCase
+{
+    /**
+     * @return array<array<string|int|array<string>>>
+     */
+    public static function numbersFromSeedProvider(): array
+    {
+        return [
+            ['xyz', 0, ['0']],
+            ['xyz', 2, ['0']],
+            ['xyz', 7, ['68']],
+            ['xyz', 8, ['196']],
+            ['xyz', 520, [
+                '1732501504205220402900929820446308723705652945081825598593993913145942097001127020633138020218038968109094917857329663184563374015879596834703721749398989648',
+                '2207401303665503434031531355511922974889692817601183500259263742625914061046146142929376778072827450461936300533206904979740474482058840003720379960491023511',
+                '1883889587903519477357838514223953979954201344665681798367023196328721975720052153913582122151913785273222921786889836987731296728825119604809609410157987402',
+                '1423259849467217711185874799515607842842602785767879766623736284680209832704638390900412597196948750015976271793930713744890547611655064835165883323889981463',
+            ]],
+        ];
+    }
+
+    /**
+     * @param string[] $results
+     */
+    #[DataProvider('numbersFromSeedProvider')]
+    public function testKeyDerivation(string $seed, int $bitLength, array $results): void
+    {
+        $numbersFromSeed = new NumbersFromSeed($seed, $bitLength);
+
+        foreach ($results as $result) {
+            $number = $numbersFromSeed->number();
+            $numberStr = gmp_strval($number);
+            $this->assertEquals($result, $numberStr);
+            $numbersFromSeed = $numbersFromSeed->iterate();
+        }
+    }
+}
