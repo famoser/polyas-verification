dd609b002a39d5daa57096ce1cdf3b1b3a8bd8d1
Florian Moser <git@famoser.ch>
2023-08-05 17:30:00 +0200

    refactor: Improve reusability of UI

--- client/components/view/CheckView.vue
+++ client/components/view/CheckView.vue
@@ -5,24 +5,25 @@ import { computed, ref } from 'vue'
 import ExpandedIndicator from '@/components/shared/ExpandedIndicator.vue'
 
 const props = defineProps<{
+  prefix: string
   entry: string
-  success: boolean
+  success?: boolean
 }>()
 
 const { t } = useI18n()
-const prefix = `domain.verification_result.${props.entry}`
+const entryPrefix = `${props.prefix}.${props.entry}`
 const expanded = ref(false)
 const showBody = computed(() => expanded.value || !props.success)
 </script>
 
 <template>
-  <div class="card shadow-sm" :class="{ 'border-success': success, 'border-danger': !success }" role="button" @click="expanded = !expanded">
+  <div class="card shadow-sm" :class="{ 'border-success': success === true, 'border-warning': success === undefined, 'border-danger': success === false }" role="button" @click="expanded = !expanded">
     <div class="card-header" :class="{ 'border-bottom-0': !showBody }">
       <div class="d-flex flex-row">
         <p class="mb-0">
           <b>
             <SuccessIndicator :success="success" class="me-2" />
-            {{ t(`${prefix}.title`) }}
+            {{ t(`${entryPrefix}.title`) }}
           </b>
         </p>
         <div class="ms-auto">
@@ -32,14 +33,17 @@ const showBody = computed(() => expanded.value || !props.success)
     </div>
     <div class="card-body" v-if="showBody">
       <p>
-        {{ t(`${prefix}.description`) }}
+        {{ t(`${entryPrefix}.description`) }}
       </p>
-      <p v-if="success" class="mb-0 alert alert-success">
-        {{ t(`${prefix}.success`) }}
+      <p v-if="success === true" class="mb-0 alert alert-success">
+        {{ t(`${entryPrefix}.success`) }}
+      </p>
+      <p v-else-if="success === undefined" class="mb-0 alert alert-warning">
+        {{ t(`${prefix}.receipt_status.unknown`) }}
       </p>
       <p v-else class="mb-0 alert alert-danger">
-        <b>{{ t(`${prefix}.failed`) }}</b>
-        {{ t(`${prefix}.failed_hint`) }}
+        <b>{{ t(`${entryPrefix}.failed`) }}</b>
+        {{ t(`${entryPrefix}.failed_hint`) }}
       </p>
     </div>
   </div>
