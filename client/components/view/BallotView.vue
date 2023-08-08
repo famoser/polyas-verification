ee87215cba4223abf4c230b98c2260cdd637e923
Florian Moser <git@famoser.ch>
2023-08-08 17:16:05 +0200

    feat: Add ballot definition structure

---
+++ client/components/view/BallotView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { api } from '@/services/api'
+import type { BallotDefinition } from '@/components/domain/BallotDefinition'
+
+defineProps<{
+  choice: string
+}>()
+
+const ballotDefinitions = ref<BallotDefinition[]>()
+
+api.getBallotDefinitions().then((result) => (ballotDefinitions.value = result))
+</script>
+
+<template>
+  <p>Here is your vote</p>
+</template>
