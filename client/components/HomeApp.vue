080771319c37f19e21a8bfd03f6cae7c1cc8aa59
Florian Moser <git@famoser.ch>
2023-08-13 14:23:19 +0200

    feat: Improve verify input flow

--- client/components/HomeApp.vue
+++ client/components/HomeApp.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import OptionView from '@/components/view/OptionView.vue'
 import { useI18n } from 'vue-i18n'
-import BallotsView from '@/components/view/BallotsView.vue'
 
 const { t } = useI18n()
 </script>
@@ -10,5 +9,4 @@ const { t } = useI18n()
   <h3 class="mb-3">{{ t('view.home_app.verifications') }}</h3>
   <OptionView class="mb-2" prefix="verify" to="/verify" />
   <OptionView prefix="receipt" to="/receipt" />
-  <BallotsView choice="00000001" />
 </template>
--- client/components/action/SetPassword.vue
+++ client/components/action/SetPassword.vue
@@ -1,31 +1,41 @@
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n'
-import { ref, watch } from 'vue'
+import { computed, onMounted, ref, watch } from 'vue'
 
 const emit = defineEmits<{
   (e: 'changed', result: string): void
 }>()
 
+const passwordInput = ref<HTMLElement>()
 const password = ref<string>()
+const sanitizedPassword = computed(() => password.value?.replace(/[^0-9.]/g, ''))
 watch(password, () => {
-  if (!password.value) {
+  if (!sanitizedPassword.value || sanitizedPassword.value.length !== 6) {
     return
   }
 
-  const cleanedUp = password.value.replace(/[^0-9.]/g, '')
-  if (cleanedUp.length !== 6) {
-    return
-  }
+  emit('changed', sanitizedPassword.value)
+})
 
-  emit('changed', cleanedUp)
+const invalidCharacters = computed(() => password.value && sanitizedPassword.value?.length !== password.value.length)
+
+onMounted(() => {
+  passwordInput.value?.focus()
 })
 
 const { t } = useI18n()
 </script>
 
 <template>
-  <div>
-    <input v-model="password" type="text" class="form-control mw-10em form-control-lg" :placeholder="t('action.set_password.set_password')" />
+  <div class="d-flex flex-column align-items-center">
+    <input
+      ref="passwordInput"
+      type="text"
+      class="form-control mw-10em form-control-lg text-center"
+      :placeholder="t('action.set_password.set_password')"
+      v-model="password"
+      :class="{ 'is-invalid': invalidCharacters }"
+    />
     <div class="form-text">
       {{ t('action.set_password.one_time_password') }}
     </div>
