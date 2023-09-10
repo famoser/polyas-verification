645c6bde192220e38831dd23f93bb3f9d12ff6b4
Florian Moser <git@famoser.ch>
2023-09-10 10:44:01 +0200

    feat: Add reset button to verify view

--- client/build/fontawesome.ts
+++ client/build/fontawesome.ts
@@ -1,7 +1,7 @@
 import { config, library as FontawesomeLibrary } from '@fortawesome/fontawesome-svg-core'
 import { faCircleCheck, faCircleQuestion, faCircleXmark } from '@fortawesome/free-regular-svg-icons'
-import { faChevronDown, faChevronRight, faSpinner } from '@fortawesome/free-solid-svg-icons'
+import { faChevronDown, faChevronRight, faRotate, faSpinner } from '@fortawesome/free-solid-svg-icons'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 
 config.autoAddCss = false
-FontawesomeLibrary.add(faCircleCheck, faCircleXmark, faCircleQuestion, faChevronRight, faChevronDown, faSpinner)
+FontawesomeLibrary.add(faCircleCheck, faCircleXmark, faCircleQuestion, faChevronRight, faChevronDown, faSpinner, faRotate)
