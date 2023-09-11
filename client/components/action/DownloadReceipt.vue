37637884e87662741c44321b09ef4b2d05174007
Florian Moser <git@famoser.ch>
2023-09-11 08:42:26 +0200

    maint: Code style improvements

--- client/components/action/DownloadReceipt.vue
+++ client/components/action/DownloadReceipt.vue
@@ -3,7 +3,7 @@ import { useI18n } from 'vue-i18n'
 import InfoPopover from '@/components/shared/InfoPopover.vue'
 import type { Receipt } from '@/components/domain/Status'
 import { api } from '@/services/api'
-import { onMounted, ref, watch } from 'vue'
+import { ref, watch } from 'vue'
 
 const props = defineProps<{ receipt: Receipt }>()
 
@@ -11,16 +11,16 @@ const { t } = useI18n()
 
 const receiptHref = ref<string>()
 
-const downloadReceipt = () => {
-  console.log('receipt download')
-  api.postDownloadReceipt(props.receipt).then((data) => {
-    const blob = new File([data], 'receipt.pdf', { type: 'application/pdf' })
-    receiptHref.value = URL.createObjectURL(blob)
-  })
-}
-
-onMounted(downloadReceipt)
-watch(props.receipt, downloadReceipt)
+watch(
+  props.receipt,
+  () => {
+    api.postDownloadReceipt(props.receipt).then((data) => {
+      const blob = new File([data], 'receipt.pdf', { type: 'application/pdf' })
+      receiptHref.value = URL.createObjectURL(blob)
+    })
+  },
+  { immediate: true }
+)
 </script>
 
 <template>
