822aca9b1c712c57205caef1f8da2b791a812576
Florian Moser <git@famoser.ch>
2026-06-04 12:41:05 +0200

    maint: Allow unsafe key generation for testing

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,10 +4,3 @@ use Famoser\PolyasVerification\Crypto\RSA;
 use Famoser\PolyasVerification\Test\Crypto\RSATest;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
-
-// required for GitHub CI on which first key generation fails
-try {
-    RSA\KeyFactory::generateRSAKey(RSATest::TEST_RSA_KEY_BITS);
-} catch (Exception $exception) {
-    var_dump("Warning: RSA Key generation failed");
-}
