e8c7d8dc387419d8c35073f16bdd81604d258341
Florian Moser <git@famoser.ch>
2026-07-29 12:52:59 +0200

    maint: Replace noty by nano-noty

--- client/services/notifiers.ts
+++ client/services/notifiers.ts
@@ -1,8 +1,8 @@
-import Noty from 'noty'
+import Noty from 'nano-noty'
 
 Noty.overrideDefaults({
-  theme: 'bootstrap-v4',
-  type: 'success'
+    theme: 'bootstrap-v5',
+    type: 'success'
 })
 
 const displayError = function (errorMessage: string) {
