6530cedd58db374f1dd335d560b120f1556565f3
Florian Moser <git@famoser.ch>
2026-08-02 09:48:15 +0200

    feat: Verify ballot owner

--- client/components/action/VerifyBallotOwner.vue
+++ client/components/action/VerifyBallotOwner.vue
@@ -1,16 +1,26 @@
 <script setup lang="ts">
 import { useTranslator } from '@/locales/translator'
+import { computed, onMounted, ref } from 'vue'
 
-defineProps<{
-  ownerId: string
-  decision?: boolean
+const props = defineProps<{
+  expectedOwnerId: string
+  verified?: boolean
 }>()
 
 const emit = defineEmits<{
   (e: 'verified', result: boolean): void
 }>()
 
-const verify = function (result: boolean) {
+const ownerInput = ref<HTMLElement>()
+const ownerInputType = computed(() => (/^\d+$/.test(props.expectedOwnerId) ? 'number' : 'text'))
+const owner = ref<string|number>()
+
+onMounted(() => {
+  ownerInput.value?.focus()
+})
+
+const verify = function () {
+  const result = owner.value === props.expectedOwnerId
   emit('verified', result)
 }
 
@@ -18,17 +28,27 @@ const { t } = useTranslator()
 </script>
 
 <template>
-  <p class="text-body-emphasis mb-2">{{ t('action.verify_ballot_owner.question') }}</p>
-  <input type="text" class="form-control form-control-lg mb-2" disabled :value="ownerId" />
-  <template v-if="decision === undefined">
-    <button class="btn btn-success me-2" @click="verify(true)">
-      {{ t('shared.yes') }}
-    </button>
-    <button class="btn btn-danger" @click="verify(false)">
-      {{ t('shared.no') }}
+  <div class="d-flex flex-column align-items-center">
+    <input
+      ref="ownerInput"
+      :type="ownerInputType"
+      class="form-control mw-15em form-control-lg text-center"
+      :placeholder="t('action.verify_ballot_owner.set_ballot_owner')"
+      v-model="owner"
+      :disabled="verified !== undefined"
+      :class="{ 'is-invalid': verified !== undefined && !verified }"
+    />
+    <div class="form-text">
+      {{ t('action.verify_ballot_owner.ballot_owner_source') }}
+    </div>
+  </div>
+
+  <template v-if="verified === undefined">
+    <button class="btn btn-primary me-2" @click="verify()">
+      {{ t('shared.verify') }}
     </button>
   </template>
-  <p v-else-if="decision" class="alert alert-success mb-0">
+  <p v-else-if="verified" class="alert alert-success mb-0">
     {{ t('action.verify_ballot_owner.successful') }}
   </p>
   <p v-else class="alert alert-danger mb-0">
@@ -37,7 +57,7 @@ const { t } = useTranslator()
 </template>
 
 <style>
-.mw-10em {
-  width: 10em;
+.mw-15em {
+  width: 15em;
 }
 </style>
